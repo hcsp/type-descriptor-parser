5a773c58d2512436a33603be6a492768270c1340
lewis <lingyidx@gmail.com>
2020-03-01 02:06:35 +0800

    feat: 完成method 解析

--- src/main/java/com/github/hcsp/descriptorparser/MethodDescriptor.java
+++ src/main/java/com/github/hcsp/descriptorparser/MethodDescriptor.java
@@ -1,10 +1,9 @@
 package com.github.hcsp.descriptorparser;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 /**
  * 代表方法的描述符，如给定方法描述符(IDLjava/lang/Thread;)Ljava/lang/Object;
@@ -18,7 +17,93 @@ public class MethodDescriptor implements TypeDescriptor {
     private String descriptor;
     private String name;
 
+    public static final String END_POINT = "\\)";
+
     public MethodDescriptor(String descriptor) {
+        String[] array = descriptor.split(END_POINT);
+
+        this.descriptor = descriptor;
+        this.returnType = TypeDescriptor.parse(array[1]);
+
+        char[] descArray = array[0].toCharArray();
+
+        for(int i = 0 ;i<descArray.length; i++){
+            switch (descArray[i]){
+                case '(':
+                    break;
+                case ')':
+                    break;
+
+
+                case 'L': {
+
+                    StringBuffer stringBuffer = new StringBuffer();
+                    while ( i < descArray.length ) {
+                        stringBuffer.append(descArray[i]);
+                        if(isEndOfReferenceType(descArray[i])){
+                            break;
+                        }
+                        i++;
+                    }
+
+                    paramTypes.add(TypeDescriptor.parse(stringBuffer.toString()));
+                    break;
+                }
+
+
+                case '[': {
+                    StringBuffer stringBuffer = new StringBuffer();
+
+                    while ( i < descArray.length) {
+                        stringBuffer.append(descArray[i]);
+                        boolean isPrimitiveType = PrimitiveTypeDescriptor.isPrimitive(Character.toString(descArray[i]));
+                        if (isPrimitiveType){
+                            break;
+                        }
+                        if (isEndOfReferenceType(descArray[i])){
+                            break;
+                        }
+                        i++;
+                    }
+
+                    paramTypes.add(TypeDescriptor.parse(stringBuffer.toString()));
+                    break;
+                }
+
+
+                default:
+                    paramTypes.add(TypeDescriptor.parse(Character.toString(descArray[i])));
+                    break;
+            }
+        }
+
+        StringBuffer paramsDesc = new StringBuffer();
+        paramsDesc.append(this.returnType.getName());
+        paramsDesc.append(" (");
+
+        if (!paramTypes.isEmpty()) {
+            boolean firstFlag = true;
+            for (TypeDescriptor typeDescriptor:paramTypes){
+                if (firstFlag){
+                    paramsDesc.append(typeDescriptor.getName());
+                    firstFlag = false;
+                }else {
+                    paramsDesc.append(", "+typeDescriptor.getName());
+                }
+            }
+        }
+
+        paramsDesc.append(")");
+        this.name = paramsDesc.toString();
+    }
+
+    /**
+     * 是否是引用类型结束
+     * @param c
+     * @return
+     */
+    private boolean isEndOfReferenceType(char c) {
+        return c == ';';
     }
 
 
