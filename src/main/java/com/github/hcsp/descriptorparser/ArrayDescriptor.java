1b23dbc5d1b1a5b29890bfa445c8c22954d54e71
lewis <lingyidx@gmail.com>
2020-02-29 23:54:27 +0800

    feat: 完成 数组/基础类型/引用类型 解析

--- src/main/java/com/github/hcsp/descriptorparser/ArrayDescriptor.java
+++ src/main/java/com/github/hcsp/descriptorparser/ArrayDescriptor.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.descriptorparser;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 数组类型的描述符，如输入[[Ljava/lang/Object;
  * 得到的name是java.lang.Object[][]
@@ -14,8 +17,51 @@ public class ArrayDescriptor implements TypeDescriptor {
     private int dimension;
     private TypeDescriptor rawType;
 
+
+    public static final String CHARACTER_REGEX = "\\[";
+
+    public static final String REFERENCE_TYPE_PREFIX = "L";
+
+    public static final String ARRAY_SUFFIX = "[]";
+
+    private static final Pattern arrayPattern;
+
+    static {
+        arrayPattern = Pattern.compile(CHARACTER_REGEX);
+    }
+
+
     // [[Ljava/lang/Object;
     public ArrayDescriptor(String descriptor) {
+        this.descriptor = descriptor;
+
+        String arrayDimension = buildArrayDimensionDesc(descriptor);
+
+        String dataTypeDesc = dimension > 0 ? descriptor.substring(dimension) : descriptor;
+
+        if (dataTypeDesc.startsWith(REFERENCE_TYPE_PREFIX)) {
+            this.rawType =  new ReferenceDescriptor(dataTypeDesc);
+        } else {
+            this.rawType = PrimitiveTypeDescriptor.of(dataTypeDesc);
+        }
+
+        this.name = rawType.getName() + arrayDimension;
+    }
+
+    /**
+     * 构造数组维度
+     * @param descriptor
+     * @return 返回构造好的数组维度描述
+     */
+    private String buildArrayDimensionDesc(String descriptor) {
+        Matcher matcher = arrayPattern.matcher(descriptor);
+
+        StringBuffer arrayDimensionDesc = new StringBuffer(1 << 5);
+        while (matcher.find()) {
+            dimension++;
+            arrayDimensionDesc.append(ARRAY_SUFFIX);
+        }
+        return arrayDimensionDesc.toString();
     }
 
     @Override
