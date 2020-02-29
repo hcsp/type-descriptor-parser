9c6fbb210c196d2baccaf53796f21f39848482c4
pathjh <1639925487@qq.com>
2020-03-01 01:46:20 +0800

    jvm的秘密

--- src/main/java/com/github/hcsp/descriptorparser/ArrayDescriptor.java
+++ src/main/java/com/github/hcsp/descriptorparser/ArrayDescriptor.java
@@ -4,7 +4,7 @@ package com.github.hcsp.descriptorparser;
  * 数组类型的描述符，如输入[[Ljava/lang/Object;
  * 得到的name是java.lang.Object[][]
  * dimension为2（二维数组）
- * rawType代表无数组的原始类型
+ * rawType代表无数组的原始类型  返回人类可读的类型名，如int或者java.lang.Object[]
  */
 public class ArrayDescriptor implements TypeDescriptor {
     // [ -> 1
@@ -14,8 +14,36 @@ public class ArrayDescriptor implements TypeDescriptor {
     private int dimension;
     private TypeDescriptor rawType;
 
+
     // [[Ljava/lang/Object;
     public ArrayDescriptor(String descriptor) {
+        char[] res=descriptor.trim().toCharArray();
+        for (int i = 0; i <res.length ;i++ ) {
+            if(res[i]=='['){
+                dimension++;
+            }
+            if(res[i]=='/'){
+                res[i]='.';
+            }
+        }
+        switch(res[dimension]){
+            case 'L':name=String.valueOf(res).substring(dimension+1,res.length-1);break;
+            case 'I':name="int";break;
+            case 'B':name="byte";break;
+            case'C':name="char";break;
+            case 'D':name="double";break;
+            case 'F':name="float";break;
+            case 'J':name="long";break;
+            case 'S':name="short";break;
+            default:name="boolean";
+        }
+
+
+        for(int j=0;j<dimension;j++){ //根据数组维度加方括号
+            name+="[]";
+        }
+
+
     }
 
     @Override
@@ -23,16 +51,16 @@ public class ArrayDescriptor implements TypeDescriptor {
         return name;
     }
 
-    public int getDimension() {
+    public int getDimension() {//数组维度
         return dimension;
     }
 
-    public TypeDescriptor getRawType() {
+    public TypeDescriptor getRawType() {//无数组的原始类型
         return rawType;
     }
 
     @Override
-    public String getDescriptor() {
+    public String getDescriptor() {//JVM中的描述符
         return descriptor;
     }
 }
--- src/main/java/com/github/hcsp/descriptorparser/PrimitiveTypeDescriptor.java
+++ src/main/java/com/github/hcsp/descriptorparser/PrimitiveTypeDescriptor.java
@@ -3,7 +3,7 @@ package com.github.hcsp.descriptorparser;
 /**
  * 代表原生类型的描述符
  */
-public enum PrimitiveTypeDescriptor implements TypeDescriptor {
+public enum PrimitiveTypeDescriptor implements TypeDescriptor {//原始类型枚举
     BYTE("B"),
     CHAR("C"),
     DOUBLE("D"),
@@ -20,11 +20,33 @@ public enum PrimitiveTypeDescriptor implements TypeDescriptor {
     }
 
     public static PrimitiveTypeDescriptor of(String descriptor) {
-        return null;
+        switch(descriptor){
+            case "B":return PrimitiveTypeDescriptor.BYTE;
+            case"C":return PrimitiveTypeDescriptor.CHAR;
+            case "D":return PrimitiveTypeDescriptor.DOUBLE;
+            case "F":return PrimitiveTypeDescriptor.FLOAT;
+            case "I":return PrimitiveTypeDescriptor.INT;
+            case "J":return PrimitiveTypeDescriptor.LONG;
+            case "S":return PrimitiveTypeDescriptor.SHORT;
+            case "Z":return PrimitiveTypeDescriptor.BOOLEAN;
+            default:return PrimitiveTypeDescriptor.VOID;
+        }
     }
 
     public static boolean isPrimitive(String descriptor) {
-        return false;
+        switch(descriptor){
+            case "B":;
+            case"C":;
+            case "D":;
+            case "F":;
+            case "I":;
+            case "J":;
+            case "S":;
+            case "Z":;
+            case"V":return true;
+
+            default:return false;
+        }
     }
 
     @Override
