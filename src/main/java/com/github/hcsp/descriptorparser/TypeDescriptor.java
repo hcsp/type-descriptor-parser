9279f7d1cb1974faccbe2881ec67da7b2119a125
keukauh <43810129+keukauh@users.noreply.github.com>
2020-03-01 17:27:13 +0800

    给定方法描述符解析为日常格式v1.0 (#6)
    给定方法描述符解析为日常格式v1.0 (#6)

--- src/main/java/com/github/hcsp/descriptorparser/TypeDescriptor.java
+++ src/main/java/com/github/hcsp/descriptorparser/TypeDescriptor.java
@@ -18,9 +18,18 @@ public interface TypeDescriptor {
     /**
      * 解析一个描述符，根据其具体类型返回不同的子类
      * @param descriptor
-     * @return PrimitiveTypeDescriptor/ReferenceDescriptor/MethodDescriptor/PrimitiveTypeDescriptor
+     * @return ArrayDescriptor/ReferenceDescriptor/MethodDescriptor/PrimitiveTypeDescriptor
      */
     static TypeDescriptor parse(String descriptor) {
+        char firstChacarter = descriptor.charAt(0);
+        switch (firstChacarter){
+            case '[' :
+                return new ArrayDescriptor(descriptor);
+            case 'L' :
+                return new ReferenceDescriptor(descriptor);
+            case '(' :
+                return new MethodDescriptor(descriptor);
+        }
         return null;
     }
 }
