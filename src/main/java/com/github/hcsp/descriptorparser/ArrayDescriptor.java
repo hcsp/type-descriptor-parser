018abba0e9c84d9701761c2c73bdfa7f68df4c81
arsonal <897386402@qq.com>
2020-05-16 11:07:20 +0800

    type descriptor parser

--- src/main/java/com/github/hcsp/descriptorparser/ArrayDescriptor.java
+++ src/main/java/com/github/hcsp/descriptorparser/ArrayDescriptor.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.descriptorparser;
 
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 数组类型的描述符，如输入[[Ljava/lang/Object;
  * 得到的name是java.lang.Object[][]
@@ -14,8 +18,22 @@ public class ArrayDescriptor implements TypeDescriptor {
     private int dimension;
     private TypeDescriptor rawType;
 
+    Pattern arrayPattern = Pattern.compile("^(\\[*)((L.*?;)*(\\w)*)");
+
     // [[Ljava/lang/Object;
     public ArrayDescriptor(String descriptor) {
+        this.descriptor = descriptor;
+        this.name = "";
+        final Matcher matcher = arrayPattern.matcher(descriptor);
+        if (matcher.find()) {
+            final String arrayDesc = matcher.group(1);
+            final String typeDesc = matcher.group(2);
+            this.dimension = arrayDesc.length();
+
+            final Map<String, Object> nameTypeMap = TypeDescriptorUtil.assignArrayName(typeDesc, this.dimension);
+            this.name = ((String) nameTypeMap.get("name"));
+            this.rawType = ((TypeDescriptor) nameTypeMap.get("typeDescriptor"));
+        }
     }
 
     @Override
--- src/main/java/com/github/hcsp/descriptorparser/MethodDescriptor.java
+++ src/main/java/com/github/hcsp/descriptorparser/MethodDescriptor.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 代表方法的描述符，如给定方法描述符(IDLjava/lang/Thread;)Ljava/lang/Object;
@@ -18,11 +19,85 @@ public class MethodDescriptor implements TypeDescriptor {
     private String descriptor;
     private String name;
 
+    Pattern methodPattern = Pattern.compile("^\\((((\\[*)(\\w*))*?)(((\\[*)(L.*?;)*)*)\\)(\\[*)((L.*?;)*(\\w)*)");
+
     public MethodDescriptor(String descriptor) {
+        final Matcher matcher = methodPattern.matcher(descriptor);
+
+        if (matcher.find()) {
+            paramTypes = primitiveArrayMixedParsing(matcher);
+
+            paramTypes.addAll(referenceArrayMixedParsing(matcher));
+
+            final String paramString = paramTypes.stream()
+                    .map(item -> item.getName())
+                    .collect(Collectors.joining(", "));
+            ArrayDescriptor arrayDescriptor = returnTypeParsing(matcher);
+
+            this.name = arrayDescriptor.getName() + " (" + paramString + ")";
+        }
+    }
+
+    private ArrayDescriptor returnTypeParsing(Matcher matcher) {
+        final String returnArray = matcher.group(9) == null ? "" : matcher.group(9);
+        String rawReturnTypeName = matcher.group(10) == null ? "" : matcher.group(10);
+        return new ArrayDescriptor(returnArray + rawReturnTypeName);
+    }
+
+    private List<TypeDescriptor> referenceArrayMixedParsing(Matcher matcher) {
+        final String rawParamTypes2 = matcher.group(5);
+        List<TypeDescriptor> result = new ArrayList<>();
+        if (rawParamTypes2.length() > 0) {
+            final List<TypeDescriptor> referenceArrayDescriptors = Stream.of(rawParamTypes2.split(";"))
+                    .map(item -> item + ";")
+                    .map(ArrayDescriptor::new)
+                    .collect(Collectors.toList());
+            result.addAll(referenceArrayDescriptors);
+        }
+        return result;
+    }
+
+    private List<TypeDescriptor> primitiveArrayMixedParsing(Matcher matcher) {
+        final String rawParamTypes1 = matcher.group(1);
+        List<TypeDescriptor> result = new ArrayList<>();
+        if (rawParamTypes1.length() > 0) {
+            int j = 0;
+            List<String> paramTypeStrings = new ArrayList<>();
+            String arrayDesc = "";
+            for (int i = 0; i < rawParamTypes1.length(); i++) {
+                String paramType = String.valueOf(rawParamTypes1.charAt(i));
+                if ("[".equals(paramType) || j != 0) {
+                    if (!"[".equals(paramType)) {
+                        arrayDesc += paramType;
+                        j = 0;
+                        paramTypeStrings.add(arrayDesc);
+                        arrayDesc = "";
+                    } else {
+                        arrayDesc += paramType;
+                        j = i;
+                    }
+                } else {
+                    j = 0;
+                    paramTypeStrings.add(paramType);
+                }
+            }
+            final List<TypeDescriptor> primitiveArrayTypeDescriptors = paramTypeStrings.stream()
+                    .map(item -> {
+                        if (item.contains("[")) {
+                            return new ArrayDescriptor(item);
+                        } else {
+                            return PrimitiveTypeDescriptor.of(item);
+                        }
+                    })
+                    .collect(Collectors.toList());
+            result.addAll(primitiveArrayTypeDescriptors);
+        }
+        return result;
     }
 
 
     public List<TypeDescriptor> getParamTypes() {
+
         return paramTypes;
     }
 
