812efd0095b0edbe8df6e56d8ff9a2d8c480e6d7
gofocus <go_focus@live.cn>
2020-05-28 17:28:44 +0800

    正则综合练习 (#25)
    正则综合练习 (#25)

--- src/main/java/com/github/hcsp/descriptorparser/ReferenceDescriptor.java
+++ src/main/java/com/github/hcsp/descriptorparser/ReferenceDescriptor.java
@@ -11,6 +11,8 @@ public class ReferenceDescriptor implements TypeDescriptor {
     private String descriptor;
 
     public ReferenceDescriptor(String descriptor) {
+        this.descriptor = descriptor;
+        this.fqcn = descriptor.substring(1).replaceAll("/", "\\.").replaceAll(";","");
     }
 
     @Override
