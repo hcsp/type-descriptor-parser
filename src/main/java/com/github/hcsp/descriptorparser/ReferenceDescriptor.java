05403948d035cbf29bdf6658a83186c5bcec2d61
kongmu373 <62830983+kongmu373@users.noreply.github.com>
2020-06-17 01:18:36 +0800

    完成JVM/正则表达式/面向对象综合练习. (#28)
    完成JVM/正则表达式/面向对象综合练习. (#28)

--- src/main/java/com/github/hcsp/descriptorparser/ReferenceDescriptor.java
+++ src/main/java/com/github/hcsp/descriptorparser/ReferenceDescriptor.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.descriptorparser;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 代表引用类型的描述符号
  */
@@ -9,8 +12,14 @@ public class ReferenceDescriptor implements TypeDescriptor {
      */
     private String fqcn;
     private String descriptor;
+    private static final Pattern PATTERN = Pattern.compile("^L((?:[a-z][a-zA-Z0-9_]*/)*[$_a-zA-Z][$_a-zA-Z0-9]*);$");
 
     public ReferenceDescriptor(String descriptor) {
+        this.descriptor = descriptor;
+        Matcher matcher = PATTERN.matcher(descriptor);
+        if (matcher.find()) {
+            this.fqcn = matcher.group(1).replaceAll("/", ".");
+        }
     }
 
     @Override
@@ -21,5 +30,9 @@ public class ReferenceDescriptor implements TypeDescriptor {
     @Override
     public String getDescriptor() {
         return descriptor;
+    }
+
+    public static boolean isReference(String descriptor) {
+        return PATTERN.matcher(descriptor).find();
     }
 }
