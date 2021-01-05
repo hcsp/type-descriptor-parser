155de87d35f240e54a91a9367d2a40a9b3e72bf3
Yuanmeng Xia <xymreasonxxs@163.com>
2021-01-05 17:16:29 +0800

    regex 附加题 (#32)
    regex 附加题 (#32)

--- src/main/java/com/github/hcsp/descriptorparser/MethodDescriptor.java
+++ src/main/java/com/github/hcsp/descriptorparser/MethodDescriptor.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 代表方法的描述符，如给定方法描述符(IDLjava/lang/Thread;)Ljava/lang/Object;
@@ -19,8 +20,27 @@ public class MethodDescriptor implements TypeDescriptor {
     private String name;
 
     public MethodDescriptor(String descriptor) {
+        this.descriptor = descriptor;
+        Pattern pattern = Pattern.compile("\\((.*)\\)(.*)");
+        Matcher matcher = pattern.matcher(descriptor);
+        if (matcher.find()) {
+            String[] paramTypesArr = matcher.group(1).split(";");
+            Pattern paramTypePattern = Pattern.compile("(L.*|\\[+(?:[BCDFIJSZV]|L.*)|[BCDFIJSZV])");
+            Stream.of(paramTypesArr).forEach(paramTypeStr -> {
+                Matcher paramMatcher = paramTypePattern.matcher(paramTypeStr);
+                while (paramMatcher.find()) {
+                    int i = 1;
+                    paramTypes.add(TypeDescriptor.parse(paramMatcher.group(i++)));
+                }
+            });
+            returnType = TypeDescriptor.parse(matcher.group(2));
+        }
+        StringBuilder methodName = new StringBuilder();
+        methodName.append(returnType.getName() + " (");
+        String paramTypeStr = paramTypes.stream().map(TypeDescriptor::getName).collect(Collectors.joining(", "));
+        methodName.append(paramTypeStr + ")");
+        name = methodName.toString();
     }
-
 
     public List<TypeDescriptor> getParamTypes() {
         return paramTypes;
--- src/main/java/com/github/hcsp/descriptorparser/TypeDescriptor.java
+++ src/main/java/com/github/hcsp/descriptorparser/TypeDescriptor.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.descriptorparser;
 
+import java.util.Arrays;
+
 public interface TypeDescriptor {
     /**
      * 返回人类可读的类型名，如int或者java.lang.Object[]
@@ -17,11 +19,21 @@ public interface TypeDescriptor {
 
     /**
      * 解析一个描述符，根据其具体类型返回不同的子类
+     *
      * @param descriptor
      * @return PrimitiveTypeDescriptor/ReferenceDescriptor/MethodDescriptor/PrimitiveTypeDescriptor
      */
     static TypeDescriptor parse(String descriptor) {
-        return null;
+        if (PrimitiveTypeDescriptor.isPrimitive(descriptor)) {
+            return PrimitiveTypeDescriptor.of(descriptor);
+        } else if (descriptor.startsWith("[")) {
+            return new ArrayDescriptor(descriptor);
+        } else if (descriptor.startsWith("(")) {
+            return new MethodDescriptor(descriptor);
+        } else {
+            return new ReferenceDescriptor(descriptor);
+        }
+
     }
 }
 
