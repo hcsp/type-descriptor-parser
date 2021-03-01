294df01a6b622d8c60dc5abc5398903596f21f58
xu-kai-quan <50357378+xu-kai-quan@users.noreply.github.com>
2021-03-01 22:01:47 +0800

    JVM/正则表达式/面向对象综合练习 (#35)
    JVM/正则表达式/面向对象综合练习 (#35)

--- src/main/java/com/github/hcsp/descriptorparser/ReferenceDescriptor.java
+++ src/main/java/com/github/hcsp/descriptorparser/ReferenceDescriptor.java
@@ -11,6 +11,8 @@ public class ReferenceDescriptor implements TypeDescriptor {
     private String descriptor;
 
     public ReferenceDescriptor(String descriptor) {
+        this.descriptor = descriptor;
+        fqcn = descriptor.replaceAll("[L;]", "").replaceAll("/", ".");
     }
 
     @Override
